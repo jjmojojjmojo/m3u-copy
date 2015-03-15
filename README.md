fd4d9ad1cd74b60b7fd5c820954851049689f15d
Josh Johnson <lionface.lemonface@gmail.com>
2015-03-15 11:22:12 -0400

    Fleshed out functionality

--- README.md
+++ README.md
@@ -4,7 +4,96 @@ A simple application to parse an [M3U](http://tools.ietf.org/html/draft-pantos-h
 
 The need for this comes from wanting to create playlists for my car's MP3-capable stereo system - I'd like to construct a list in an application like [Clementine](https://www.clementine-player.org/), and then easily transfer those files and the playlist itself to an SD card. 
 
-I'm also interested in using RockBox with my iPod mini. It can also be managed like a simple external drive/SD card.
+I'm also interested in using [RockBox](http://www.rockbox.org/) with my iPod mini. It can also be managed like a simple external drive/SD card.
+
+## INSTALLATION
+This application was developed on OSX. It should run well on any other unix-style operating system (e.g. Linux). Windows support is probable but untested. Binary releases will happen... someday :).
+
+Check out this repository to a useful location, like `~/m3ucopy`:
+
+```
+$ cd ~
+$ git clone https://github.com/jjmojojjmojo/m3u-copy.git
+```
+
+Install/Upgrade [boot](http://boot-clj.com/):
+
+```
+$ wget https://github.com/boot-clj/boot/releases/download/2.0.0-rc12/boot.sh
+$ mv boot.sh boot && chmod a+x boot && sudo mv boot /usr/local/bin
+```
+
+If you've installed boot before, you may need to update it:
+
+```
+$ boot -u
+```
+
+Now you can run `./m3ucopy` and start moving files.
+
+### Advanced Installation W/O Jars
+**Note:** This is theorhetical at the moment!
+
+The `m3ucopy` script can be copied anywhere, it just needs to be modified so boot knows where to find the source code:
+
+```
+$ sudo mkdir /opt/m3ucopy
+$ sudo chmod -r 775 /opt/m3ucopy
+$ git clone --depth 1 https://github.com/jjmojojjmojo/m3u-copy.git /opt/m3ucopy
+$ sudo mv /opt/m3ucopy/m3ucopy /usr/local/bin
+$ sudo chmod +x /usr/local/bin/m3ucopy
+$ sudo vi /usr/local/bin/m3ucopy
+...
+```
+
+This is the line to change:
+
+```clojure
+(set-env!
+  :source-paths #{"src"}
+  :dependencies '[[me.raynes/fs "1.4.6"]
+                  [pandect "0.5.1"]
+                  [green-tags "0.3.0-alpha"]])
+```
+
+Change the `:source-paths` setting to `#{"/opt/m3ucopy"}`. For more information on what `set-env!` does, check out [the boot documentation](https://github.com/boot-clj/boot/wiki/Boot-Environment).
+
+## USAGE
+The current incarnation of m3ucopy runs as a [boot](http://boot-clj.com/) script that uses the local codebase to do its thing. 
+
+```
+$ ./m3ucopy -h
+Usage: m3ucopy [options] [source] [dest]
+
+Options:
+  -h, --help             Print this help info.
+  -d, --dry-run          Dry-run mode. Just print what would be done to stdout
+  -a, --alg-style STYLE  Set specify the path style. Options are: same, uid, artist-album artist-album-path. Default is 'same' to STYLE.
+  -s, --spaces-ok        Are spaces OK? If not, they will be replaced with underscores. Defaults to true.
+```
+
+Dry-run mode writes the output m3u file to a temporary path, and displays what would happen to the console.
+
+Spaces-ok controls if spaces are replaced in filenames. Some other special characters ([?/\,]) are replaced in any case.
+
+The path style setting controls how paths will be created relative to the m3u file:
+
+**same**
+
+    Whatever the file was called (without regard to directory structure) will be used to name the 
+    file when copied.
+    
+**uid**
+
+    A simple md5 hash based on the tag information will be used to name the file. (e.g. 52a856749f12bfb2b6e9678cf29eeb15.mp3)
+    
+**artist-album**
+
+    The file will be named Artist - Album - Track# - Title.ext (e.g. Lucas - Lucacentric - 002 - Lucas With The Lid Off.mp3)
+    
+**artist-album-path**
+
+    The file will be put into an artist and album sub-directory. (e.g. Lucas/Lucacentric/002 - Lucas With The Lid Off.mp3)
 
 ## FEATURES
 
@@ -14,6 +103,11 @@ Avoid copying the same file if it already exists.
 ### Path mangling
 Paths in the provided M3U file need to be made relative to where the files are stored on the media.
 
+Paths can be generated as well using tagging information from the source files.
+
+### Dry-run Mode
+See what will happen without manipulating the data.
+
 ## TECH
 I'm using [clojure](http://clojure.org/), and [boot](http://boot-clj.com/) for this project.
 
@@ -21,36 +115,36 @@ Phase one will be implemented as a simple boot script.
 
 A web interface may be useful in lieu of more robust (QT/GTK/Swing) UIs in the future.
 
-If a database is used, consider bundling redis somehow, or use something like https://github.com/flausenhaus/clj-rocksdb.
+If a database is used, consider bundling redis somehow, or use something like [rocksdb](https://github.com/flausenhaus/clj-rocksdb).
 
 Another option would be to store file info as an [EDN](https://github.com/edn-format/edn) file. This should be as efficient as using a key-valyue store, but it may slow startup time, or have less robust search features (since you interact with it using normal clojure mechanisms).
 
 ## NOTE
 I'm sure something like this exists somewhere :) This project exists mostly for my own edification - this is an early hobby clojure project for me.
 
+## NEAR TERM GOALS
+The main thing is adding tests. Lots and lots of tests.
+
 ## FUTURE
 I have ideas for the near-term once the basic functionality is complete:
   
-  - use swing or another UI toolkit (QT has bindings for java) to construct a playlist manager application.
+  - use swing or another UI toolkit ([QT has bindings for java](http://briancarper.net/blog/398/)) to construct a playlist manager application.
   - add playlist generation capabilities.  
   - support other playlist formats (why?).
   - construct/use a file metadata database.
 
-## Future Features
+### Future Features
 
-### Robust UI
+#### Robust UI
 Provide a desktop and/or web UI to make using m3u-copy easier.
 
-### Simple music DB
+#### Simple music DB
 To make searching easier/faster, provide a simple searchable database.
 
 The user can import one or more directories. 
 
-### Auto-add To Music DB
+#### Auto-add To Music DB
 Run a service to monitor the source directories and automatically add/remove/update files in the database to keep it fresh.
 
-### Playlist Management
+#### Playlist Management
 Inspect, create, and modify playlists.
-
-### File Copy - Playlist
-Create a playlist in a given format (M3U) and copy it to a given target. Copy all referenced files, but don't copy files already on the target.
--- src/m3ucopy/core.clj
+++ src/m3ucopy/core.clj
@@ -1,8 +1,226 @@
 (ns m3ucopy.core
   "M3U file copier utility"
+  (:require [clojure.java.io :as io]
+            [pandect.algo.md5 :refer [md5 md5-file]]
+            [me.raynes.fs :as fs]
+            [green-tags.core :refer [get-fields]]
+            [clojure.string :as string])
   (:gen-class))
+
+(defmethod print-method java.io.File [o ^java.io.Writer w]
+  (.write w (.getPath o)))
+
+
+(defn extended?
+  [line]
+  "Return true if the line is a header indicating this is an *extended* M3U file"
+  (.contains line "#EXTM3U"))
+
+(defn extended-meta?
+  [line]
+  "Return true if the line is extended metadata, false if not"
+  (boolean (re-find #"#EXTINF" line)))
+
+^{:todo "Is this the best way to determine if the files are different?"}
+(defn copy? 
+  [source dest]
+  "Return true if dest doesn't exist, or dest is a different file, false if not.
+   Determines when to copy a file."
+  (if (fs/exists? dest)
+    (not= (md5-file source) (md5-file dest))
+    true))
+
+(defn bad-chars
+  [filename &{:keys [replacement] :or {replacement "_"}}]
+  "Given a filename, return that name with commonly bad file system chars replaced
+   with :replacement character (defaults to underscore)"
+  (string/replace filename #"[\\/?\,]" replacement))
+
+;; custom destination paths, revisit later.. 
+;; (defn expand-interp
+;;   "given the list of values and a {x} template variable, return a string with
+;;    the current value of x"
+;;   [interp values]
+;;   (let [match (re-find #"\{.+\}" interp)]
+;;     (if match
+;;       (get values match "X")
+;;       interp)))
+;; 
+;; (defn interpolate
+;;   "Given a template, replace {x} with the value of x and return the string"
+;;   [template values]
+;;   (println values)
+;;   (let [words (string/split template #"\W")
+;;         processed (map #(expand-interp %1 values) words)]
+;;     (println words)
+;;     (string/join processed " ")))
+;; 
+;; (defn template-path
+;;   [path dest-dir template]
+;;   "Create a custom destination path using tag information from the provided path"
+;;   (let [metadata (smart-tags path)
+;;         interp (interpolate template metadata)
+;;       (println "TEMPL: " (.getPath destpath))))
+
+(defn relative-to
+  "Return a path relative to a given file"
+  [path relative-to]
+  (let [absrel (.getPath (fs/absolute (fs/parent relative-to)))
+        abspath (.getPath (fs/absolute path))]
+    (string/replace (string/replace-first abspath absrel "") #"^/" "")))
+
+(defn smart-tags
+  "Based on a music file path, return a map containing essential information 
+   about the file, handles untagged information gracefully"
+   [path &{:keys [default] 
+           :or {default "Unknown"}}]
+   (let [metadata (get-fields path)]
+     {:genera (:genera metadata default)
+      :artist (:artist metadata default)
+      :album (:album metadata default)
+      :album-artist (:album-artist metadata (:artist metadata default))
+      :title (:title metadata default)
+      :extension (fs/extension path)
+      :track (format "%03d" (Integer/parseInt (:track metadata "0")))
+     }))
+   
+
+(defn uid-path
+  [path dest-dir]
+  "Create a unique short 'fingerprint' file name based on tag information. 
+   Useful for filesystems that can't handle special characters or long filenames"
+  (let [{:keys [:album-artist :album :title :track :extension]} (smart-tags path)
+        hash (md5 (str album-artist album track title))]
+       (io/file dest-dir (str hash extension))))
+    
+
+(defn artist-album-name
+  [path dest-dir]
+  "Construct a path where the base file name contains the artist, album and track number"
+  (let [{:keys [:album-artist :album :title :track :extension]} (smart-tags path)]
+    (io/file dest-dir (bad-chars (str album-artist " - " album " - " track " - " title extension)))))
+
+(defn artist-album-path
+  [path dest-dir]
+  "Create a standard 'Artist/Album/00X-Title.ext' style destination path"
+  (let [{:keys [:album-artist :album :title :track :extension]} (smart-tags path)
+       filename (bad-chars (str track " - " title extension))]
+    (io/file dest-dir (bad-chars album-artist) (bad-chars album) filename)))
+
+(defn- construct-path
+  [path dest-dir &{:keys [style] :or {style :same}}]
+  "Create a destination path for a given file"
+  (case style
+    :artist-album (artist-album-name path dest-dir)
+    :artist-album-path (artist-album-path path dest-dir)
+    :uid (uid-path path dest-dir)
+    (io/file dest-dir (fs/base-name path))))
   
-(defn -main
-    [& args]
-    (println (first args))
-    (println "WINNING"))
+(defn dest-path
+  [path dest-dir &{:keys [style spaces-ok] :or {spaces-ok true style :same}}]
+  "Construct a destination path based on the source path and destination 
+   directory"
+  (let [raw (construct-path path dest-dir :style style)]
+     (if spaces-ok
+       raw
+       (string/replace raw #"\s+" "_"))))
+    
+(defn copy-music
+  [source dest &{:keys [dry-run] :or {dry-run false}}]
+  "Copy music file to its destination, but only if it's not already there"
+  (if (copy? source dest)
+    (do 
+       (println "Copying" source "to" dest)
+       (if (not dry-run) (fs/copy+ source dest)))
+    (println dest "already exists. Skipping...")))
+
+(defn- write-line
+  [wtr line]
+  "Helper function to write a single line to the given m3u file"
+  (.write wtr (str line "\n")))
+
+(defn copy-m3u
+  [path dest &{:keys [style spaces-ok dry-run] 
+               :or {style nil 
+                    spaces-ok true 
+                    dry-run false}}]
+  "Reconstruct an m3u file from the given path to a destination directory, and
+   copy all referenced files, mangling paths so they are relative to the 
+   destination m3u file.
+   
+   keyword arguments control behavior:
+       :path-style - the algorithm to use to generate the destination path name
+                     for music files. 
+                     
+                     Options are:
+                        - uid: generate a hash based on the id3 tags - use this
+                               if the destination file system or player doesn't
+                               support long file names or special characters.
+                        - album-artist-name: artist - album - track - title.mp3
+                        - album-artist: artist/album/track-title.mp3
+                        
+                     The default is to keep the file name from the source path.
+                     
+       :spaces-ok - set to true to leave spaces in file names. Defaults to true.
+                    Spaces are replaced with underscores.
+                    
+       :dry-run - print what would be done without actually doing it. Defaults 
+                  to false. 
+       
+   
+   "
+  (if dry-run
+    (do 
+      (println "****DRY RUN MODE DETECTED.*********************************")
+      (println "    NO FILES WILL BE MANIPULATED")
+      (println)))
+   
+  (if-not (fs/exists? path)
+    (throw (Exception. (str "File " path " does not exist"))))
+  
+  (if (and (fs/exists? dest) (not (fs/directory? dest)))
+    (throw (Exception. (str "Destination path " dest " is not a directory"))))
+  
+  (if (not (fs/exists? dest))
+    (do
+      (println "Creating directory " dest)
+      (if dry-run (fs/mkdirs dest))))
+  
+  (let [temp-path (fs/temp-name "m3ucopy-")
+        dest-m3u-path (io/file dest (fs/base-name path))
+        dest-m3u (if dry-run temp-path dest-m3u-path)]
+    (println "Creating m3u file at:" dest-m3u)
+    (println "Reading m3u file from:" path)
+    (println)
+    (with-open [rdr (io/reader path)
+                wtr (io/writer dest-m3u)]
+      (let [lines (line-seq rdr)
+            extended (extended? (first lines))]
+        (if extended
+          (do
+            (write-line wtr "#EXTM3U")
+            (doseq [data (partition-all 2 (rest lines))]
+              (let [metadata (first data)
+                    filepath (second data)
+                    destpath (dest-path filepath dest :spaces-ok spaces-ok :style style)
+                    playpath (relative-to destpath dest-m3u-path)]
+                (copy-music filepath destpath :dry-run dry-run)
+                (write-line wtr metadata)
+                (write-line wtr playpath))))
+          
+          (doseq [line lines]
+            (let [filepath line
+                  destpath (dest-path filepath dest :spaces-ok spaces-ok :style style)
+                  playpath (relative-to destpath dest-m3u-path)]
+              (copy-music filepath destpath :dry-run dry-run)
+              (write-line wtr playpath))))))
+    
+    (if dry-run
+      (do
+        (println)
+        (println "******* Dumping temporary M3U file. *********")
+        (print (slurp dest-m3u))
+        (println "*********************************************\n")
+        (fs/delete dest-m3u)))))
+
+
